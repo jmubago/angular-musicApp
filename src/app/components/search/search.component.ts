b8f5ed0c1d05274aa2cfad64d0f420a2ed95b473
jmubago <jmubagosotomayor@gmail.com>
2019-03-31 09:48:21 +0200

    search component working

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  public searchResults: any[] = [];
+
+  constructor(private spotify: SpotifyService) { }
 
   ngOnInit() {
   }
 
+  public searchArtist(text: string){
+    this.spotify.getArtist(text)
+    .subscribe(data => {
+      console.log(data);
+      this.searchResults = data;
+    })
+  }
 }
