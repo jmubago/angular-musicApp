dd8283c10cb451b37206eb2fa8edf816172b24b3
jmubago <jmubagosotomayor@gmail.com>
2019-04-07 11:27:18 +0200

    handle error at home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,13 +10,20 @@ export class HomeComponent implements OnInit {
 
   public newReleases: any[] = [];
   public loading: boolean;
+  public error: boolean;
+  public errorMessage: string;
 
   constructor(private spotify: SpotifyService) { 
     this.loading = true;
+    this.error = false;
     this.spotify.getNewReleases()
     .subscribe(data => {
       this.newReleases = data;
       this.loading = false;
+    }, (errorService)=>{
+      this.loading = false;
+      this.error = true;
+      this.errorMessage = errorService.error.error.message;
     });
   }
 
